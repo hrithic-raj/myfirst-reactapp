02b5e5e83725213065c12e8bfeeda01bb0facdb2
Hrithic <hrithicraj730@gmail.com>
2024-08-23 12:43:11 +0530

    formval1

--- my-app/src/components/Formval.jsx
+++ my-app/src/components/Formval.jsx
@@ -4,36 +4,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button,Form} from 'react-bootstrap';
 
 function Formval() {
-    const initialvalue={username:'',email:'',password:''}
-    const [formvalue,setformvalue]=useState(initialvalue);
+    const initialValues={username:'',email:'',password:''}
+    const [formValues,setFormValues]=useState(initialValues);
+    const [formErrors,setFormErrors]=useState(initialValues);
+    
+    //onchange
     const handlechange=(event)=>{
         const {name,value}=event.target;
-        setformvalue({...formvalue, [name]:value});
-        console.log(formvalue);
+        setFormValues({...formValues, [name]:value});
+        console.log(formValues);
     }
+    //onSubmit
     const handlesubmit=(event)=>{
         event.preventDefault();
+        validate(formValues)
+    }
+
+    //validate
+    const validate=(values)=>{
+
     }
 
   return (
     <>
         <div className='formdis'>
         <h1 >Form <br></br>Validation</h1>
-        <pre>{JSON.stringify(formvalue,undefined,10)}</pre>
+        <pre>{JSON.stringify(formValues,undefined,10)}</pre>
         </div>
         <div className='formvalbox'>
             <form onSubmit={handlesubmit}>
                 <Form.Group>
                     <label htmlFor="username">Username</label>
-                    <Form.Control type='text' name='username' value={formvalue.username} onChange={handlechange} placeholder='Enter your username'/>
+                    <Form.Control type='text' name='username' value={formValues.username} onChange={handlechange} placeholder='Enter your username'/>
                 </Form.Group>
                 <Form.Group>
                     <label htmlFor="email">Email</label>
-                    <Form.Control type='email' name='email' value={formvalue.email} onChange={handlechange} placeholder='Enter your valid email'/>
+                    <Form.Control type='email' name='email' value={formValues.email} onChange={handlechange} placeholder='Enter your valid email'/>
                 </Form.Group>
                 <Form.Group>
                     <label htmlFor="password">Password</label>
-                    <Form.Control type='Password' name='password' value={formvalue.password} onChange={handlechange} placeholder='Enter your password'/>
+                    <Form.Control type='Password' name='password' value={formValues.password} onChange={handlechange} placeholder='Enter your password'/>
                 </Form.Group>
                 <br />
                 <Button style={{width:"200px"}}>Submit</Button>
